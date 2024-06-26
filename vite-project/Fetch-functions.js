c7ff69fe6f89705831e77a325206a6200a0a0f59
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-06-26 14:23:08 -0400

    new branch

--- vite-project/Fetch-functions.js
+++ vite-project/Fetch-functions.js
@@ -1,17 +1,19 @@
-export const fetchAllPokemon = async () => {
+import { renderPokemon } from "./render-functions";
+
+export const fetchAllPokemon = async (pokemon = "?limit=5") => {
   try {
     const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/?limit=1302`
+      `https://pokeapi.co/api/v2/pokemon/${pokemon}`
     );
     if (!response.ok) {
       throw new Error("You didn't catch 'em all");
     }
     const jsonData = await response.json();
-
+    //if (!jsonData.results) return jsonData;
     return jsonData.results;
   } catch (error) {
     console.warn(error);
-    null;
+    return null;
   }
 };
 
@@ -19,38 +21,43 @@ export const fetchAllPokemon = async () => {
 //fetch the specific one
 //grab all the information into an object
 
-const fetchDataPokemon = async (e) => {
-  // const pokeName = e.target;
-  //const response = fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`);
-  if (!response.ok) {
-    throw new Error("can't find pokemon - try again later");
-  }
-  const pokeJson = await response.json();
+// const fetchDataPokemon = async (e) => {
+//   // const pokeName = e.target;
+//   //const response = fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+//   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`);
+//   if (!response.ok) {
+//     throw new Error("can't find pokemon - try again later");
+//   }
+//   const pokeJson = await response.json();
+//   console.log("here", pokeJson);
+//   const id = await pokeJson.id;
+//   const weight = await pokeJson.weight;
+//   const namePoke = await pokeJson.name;
+//   const height = await pokeJson.height;
+//   const stats = await pokeJson.stats[0];
+//   const type = await pokeJson.types[0].type;
+//   const ability = await pokeJson.abilities;
 
-  const id = await pokeJson.id;
-  const weight = await pokeJson.weight;
-  const namePoke = await pokeJson.name;
-  const height = await pokeJson.height;
-  const stats = await pokeJson.stats[0];
-  const type = await pokeJson.types[0].type;
-  const ability = await pokeJson.abilities;
-
-  const pokeData = { id, weight, namePoke, height, stats, type, ability };
-  console.log(pokeData);
-  return pokeData;
-};
-
-
+//   const pokeData = { id, weight, namePoke, height, stats, type, ability };
+//   //console.log(pokeData);
+//   return pokeData;
+// };
 
 // // user can submit the form
-// const searchPokemon = (event) => {
-//   event.preventDefault();
-//   const pokemonName = document.getElementById('pokemon-search').value.trim();
-//   if (pokemonName) {
-//       updatePokemonDetails(pokemonName);
-//   }
-// };
+const searchPokemon = async (event) => {
+  event.preventDefault();
+  document.querySelector("#pokemon-list").innerHTML = ``;
+  const value = document.querySelector("#pokemon-search").value;
+
+  const pokemonData = await fetchAllPokemon(value);
+  //console.log(pokemonData);
+  return renderPokemon(pokemonData);
+  //   const searchedPokemon = Object.create(fetchAllPokemon(value));
+  //console.log(searchedPokemon);
+  //   renderPokemon(searchedPokemon.name);
+};
+
+document.querySelector("#poke-form").addEventListener("submit", searchPokemon);
 
 // // Make getting pokemon user accessible
 // const fetchOnePokemon = async (pokemon) => {
@@ -97,4 +104,3 @@ const fetchDataPokemon = async (e) => {
 //   };
 
 //console.log(fetchDataPokemon());
-
--- vite-project/render-functions.js
+++ vite-project/render-functions.js
@@ -1,12 +1,19 @@
 import { fetchAllPokemon } from "./Fetch-functions";
 
-const renderPokemon = async (pokemon) => {
+export const renderPokemon = async (pokemon) => {
   //make sure you clear the pokemon every time
   //document.querySelector("#pokemon-list").innerHTML = ``;
   //takes an array of objects = results
-  for (let i = 0; i < 350; i++) {
+  //console.log(typeof pokemon);
+
+  // const pokeData = { id, weight, namePoke, height, stats, type, ability };
+  //create an element for each thing
+  //append elements to cards
+  if (!pokemon) return;
+  for (let i = 0; i < pokemon.length; i++) {
     //console.log(pokemon[i]);
     const url = pokemon[i].url;
+
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`No Pokemon exist brother`);
@@ -25,21 +32,21 @@ const renderPokemon = async (pokemon) => {
     //const section = document.createElement("section");
     document.querySelector("#pokemon-list").append(idNum, li);
   }
+
   // const pokeData = { id, weight, namePoke, height, stats, type, ability };
   //create an element for each thing
   //append elements to cards
 };
 
 const myPoke = await fetchAllPokemon();
-
+console.log(myPoke);
 renderPokemon(myPoke);
 //grabIdNum(myPoke);
 
-const renderCard = (e) => {
-  if (e.target.matches("li")) console.log(e.target);
-};
+// const renderCard = (e) => {
+//   if (e.target.matches("li")) console.log(e.target);
+// };
 
-const list = document.getElementById("pokemon-list");
-console.log(list);
+// const list = document.getElementById("pokemon-list");
 
-list.addEventListener("click", renderCard);
+// list.addEventListener("click", renderCard);
